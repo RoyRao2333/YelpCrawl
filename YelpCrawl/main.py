a71c195961468b725fdd2913eaa15f4f975137cf
RoyRao2333 <royrao2333@gmail.com>
2022-03-01 18:36:05 +0800

    Finished first version

--- YelpCrawl/main.py
+++ YelpCrawl/main.py
@@ -1,12 +1,17 @@
 import os
 import csv
 import time
+import re
 from urllib.request import urlretrieve
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
+# init driver
+service = Service("/usr/local/bin/msedgedriver")
+service.start()
+driver = webdriver.Remote(service.service_url)
 
 def find_element_by_xpath(self, xpath):
     return self.find_element(by=By.XPATH, value=xpath)
@@ -20,6 +25,7 @@ def makedir(path):
 
 def save_img(url, name):
     image_folder = os.getcwd() + "/results/img/"
+    makedir(image_folder)
     image_path = os.path.join(image_folder, name + ".jpg")
     urlretrieve(url, image_path)
 
@@ -41,7 +47,7 @@ def crawl(self, file):
     if find_element_by_xpath(self, "//*[@title='Next']").get_attribute("href") is None:
         return
     else:
-        time.sleep(10)
+        time.sleep(5)
         find_element_by_xpath(self, "//*[@title='Next']").click()
         crawl(self, file)
 
@@ -52,7 +58,7 @@ def get_entrance(self):
     soup = BeautifulSoup(raw, "lxml")
     div = soup.find("div", class_="media-landing_gallery photos")
     ul = div.find("ul")
-    inner_url = ul.find("li").find("a").attrs["href"]
+    inner_url = "https://www.yelp.com" + ul.find("li").find("a").attrs["href"]
     return inner_url
 
 def parse(self, file):
@@ -63,35 +69,38 @@ def parse(self, file):
         .find("div", id="wrap")\
         .find("div", class_="arrange_unit arrange_unit--fill")\
         .find("h1")\
-        .string
+        .get_text()
     index = soup\
         .find("div", id="wrap")\
         .find("ul", class_="media-footer_inner")\
         .find("li", class_="media-footer_count")\
         .find("span", class_="media-count_current")\
-        .string
+        .get_text()
     img = soup\
         .find("div", id="wrap")\
         .find("img", class_="photo-box-img")\
         .attrs["src"]
     comment = soup\
         .find("div", id="wrap")\
-        .find("div", class_="caption selected-photo-caption-text")\
-        .string
+        .find("div", class_="caption selected-photo-caption-text")
+    comment = comment.get_text() if comment.get_text() is not None else "None"
     date = soup \
         .find("div", id="wrap")\
         .find("div", class_="selected-photo-details")\
         .find("span")\
-        .string
+        .get_text()
     passport = soup \
-        .find("div", id="wrap") \
-        .find("div", class_="media-story")
+        .find("div", id="wrap")\
+        .find("div", class_="media-info")
     username = ""
     is_merchant = ""
 
-    info = passport.find("li", class_="user-name")
+    info = passport.find("ul", class_="user-passport-info")
     if info:
-        username = info.find("a", id="dropdown_user-name").string
+        username = info\
+            .find("li", class_="user-name")\
+            .find("a", id="dropdown_user-name")\
+            .get_text()
         is_merchant = "No"
     else:
         username = passport\
@@ -99,7 +108,7 @@ def parse(self, file):
             .find("strong")\
             .find("a")\
             .find("span")\
-            .string
+            .get_text()
         is_merchant = "Yes"
 
     image_name = title + " " + index
@@ -115,11 +124,6 @@ def parse(self, file):
     print("Fetched " + image_name)
 
 if __name__ == '__main__':
-    # init driver
-    service = Service("/usr/local/bin/msedgedriver")
-    service.start()
-    driver = webdriver.Remote(service.service_url)
-
     # get paths
     makedir(os.path.join(os.getcwd(), "results"))
     output_file = open(os.path.join(os.getcwd(), "results/manifest.csv"), "w", encoding="utf-8-sig")
@@ -130,6 +134,8 @@ if __name__ == '__main__':
     # do your job
     for store_url in get_input():
         driver.get(store_url)
+        url = get_entrance(driver)
+        driver.get(url)
         crawl(driver, output_file)
 
     # deinit
